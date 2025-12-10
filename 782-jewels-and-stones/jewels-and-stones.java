2267a27440116efcd5ba40aeae2d37e9cafd226a
Dhanu Mallina <146764029+dhanumallina@users.noreply.github.com>
2025-12-11 00:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (42.02%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,21 +1,25 @@
 class Solution {
-    public int numJewelsInStones(String jewels, String stones) {
-        int count = 0;
-
-        // For every stone, check if it matches any jewel
-        for (int i = 0; i < stones.length(); i++) {
-            char stone = stones.charAt(i);
-
-            for (int j = 0; j < jewels.length(); j++) {
-                char jewel = jewels.charAt(j);
-
-                if (stone == jewel) {
-                    count++;
-                    break; // no need to check further
-                }
+    public int numJewelsInStones(String jewels, String stones) 
+    {
+    int count=0;
+    for(int i=0;i<stones.length();i++)
+    {
+        char c=stones.charAt(i);
+        boolean found=false;
+        for(int j=0;j<jewels.length();j++)
+        {
+            char ch=jewels.charAt(j);
+            if(c==ch)
+            {
+              found=true;
+              break;
             }
         }
-
-        return count;
+        if(found)
+        {
+            count=count+1;
+        }
+    }
+    return count;
     }
 }
