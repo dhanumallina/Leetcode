3d96457fafc016dee13421cb69925947bf55d9d7
M D S RAMAKRISHNA <146764029+dhanumallina@users.noreply.github.com>
2026-06-03 13:02:40 +0530

    Time: 17 ms (5.38%) | Memory: 47.1 MB (12.03%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.java
@@ -1,37 +1,24 @@
 class Solution {
-
-    public String reverse(String s) {
-        String rev = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            rev = rev + s.charAt(i);
+    public String reverse(String s){
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
         }
         return rev;
     }
-
     public int maximumNumberOfStringPairs(String[] words) {
-        int n = words.length;
-        int answer = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (words[i].equals("0")) {
-                continue;
+        int n=words.length;
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+            String a=words[i];
+            String b=words[j];
+            String revb=reverse(b);
+            if(revb.equals(a)){
+             ans++;
             }
-
-            String a = words[i];
-
-            for (int j = i + 1; j < n; j++) {
-                if (words[j].equals("0")) {
-                    continue;
-                }
-
-                if (a.equals(reverse(words[j]))) {
-                    answer++;
-                    words[i] = "0"; // mark both used
-                    words[j] = "0";
-                    break;
-                }
             }
         }
-        return answer;
+        return ans;
     }
 }
