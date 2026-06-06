01b0083e0292e8906f3638b272aa5e9aa18f05a4
M D S RAMAKRISHNA <146764029+dhanumallina@users.noreply.github.com>
2026-06-06 15:10:12 +0530

    Time: 3 ms (6.99%) | Memory: 43.4 MB (23.38%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -3,7 +3,7 @@ class Solution {
       int Hcol=colors[0];
      // int diff=0;
       int maxdiff=0;
-      for(int i=0;i<colors.length;i++){
+      for(int i=0;i<colors.length-1;i++){
         for(int j=0;j<colors.length;j++){
             if(colors[i]!=colors[j]){
                 int dist=Math.abs(i-j);
