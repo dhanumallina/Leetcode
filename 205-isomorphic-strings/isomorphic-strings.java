44742a4e47e0151e3234840e036e970f4fb8f4c6
M D S RAMAKRISHNA <146764029+dhanumallina@users.noreply.github.com>
2026-06-11 13:22:17 +0530

    Time: 15 ms (46.61%) | Memory: 43.9 MB (41.42%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -6,11 +6,11 @@ class Solution {
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
              char cc=t.charAt(i);
-            if(!hm.containsKey(c)){
+            if(!hm.containsKey(c)){ 
                 hm.put(c,i);
-            }
-            if(!hmm.containsKey(cc)){
-                hmm.put(cc,i);
+                 
+            }if(!hmm.containsKey(cc)){
+                 hmm.put(cc,i); 
             }
             if(!hmm.get(cc).equals(hm.get(c))){
               return false;
